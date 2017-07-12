2395624f864eb328ad8a0096b34d878026c0c2d4
Aztec <iorbit@protonmail.com>
2017-07-12 12:49:11 +0100

    Fixes most bugs
    Hey Daniel

---
+++ microbit_menu-v0.2.py
@@ -0,0 +1,26 @@
+from microbit import *
+import random
+menuAmount = 1  # Start at 0
+menu = 0
+void = 0
+
+while True:
+    if menu == 0:
+        if void == 0:
+            display.scroll("DICE")
+            void = 1
+    elif menu == 1:
+        if void == 0:
+            display.scroll("LETTER")
+            void = 1
+    if button_a.is_pressed():
+        menu += 1
+        if menu > menuAmount:
+            menu = 0
+        void = 0
+    if button_b.is_pressed():
+        if menu == 0:
+            display.show(str(random.randint(1, 6)))  # Random int (1 - 6)
+        elif menu == 1:
+            letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
+            display.show(random.choice(letters))  # Choose random of alphabet
