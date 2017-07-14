443bf535d15501bb9f89a6403cc775dbb45fbd06
Aztec <iorbit@protonmail.com>
2017-07-14 21:44:25 +0100

    Update microbit_menu-v0.25.py

--- microbit_menu-v0.25.py
+++ microbit_menu-v0.25.py
@@ -9,35 +9,36 @@
 '''
 from microbit import *
 import random
-menuAmount = 2  # Start at 0
-menu = 0
-void = 0
+menuAmount = 2  # Count of menus (starting at 0)
+menu = 0  # Default menu to start on
+void = 0  # If text scrolled already, don't scroll again until button pressed
 
 while True:
     if menu == 0:
         if void == 0:
-            diceImage = Image("90009:00000:00900:00000:90009")
+            diceImage = Image("90009:00000:00900:00000:90009")  # Dice image
             display.show(diceImage)
             sleep(250)
             void = 1
     elif menu == 1:
         if void == 0:
-            letterImage = Image("90000:90000:90000:90000:99990")
+            letterImage = Image("90000:90000:90000:90000:99990")  # Letter 'L'
+	
             display.show(letterImage)
             sleep(250)
             void = 1
     elif menu == 2:
         if void == 0:
-            colourImage = Image("09990:90000:90000:90000:09990")
+            colourImage = Image("09990:90000:90000:90000:09990")  # Letter 'C'
             display.show(colourImage)
             sleep(250)
             void = 1
-    if button_a.is_pressed():
+    if button_a.is_pressed():  # If menu swap button is pressed
         menu += 1
         if menu > menuAmount:
             menu = 0
         void = 0
-    if button_b.is_pressed():
+    if button_b.is_pressed():  # If function button is pressed
         if menu == 0:
             display.show(str(random.randint(1, 6)))  # Random int (1 - 6)
         elif menu == 1:
